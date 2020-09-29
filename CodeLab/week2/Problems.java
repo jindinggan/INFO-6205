633b01433b6910f2b165f0fb9666472df4fe65b1
jindinggan <dinggan_jin14@126.com>
2020-09-29 08:07:58 -0700

    Problem 1-12 finished except 6 and 8

--- CodeLab/week2/Problems.java
+++ CodeLab/week2/Problems.java
@@ -1,6 +1,9 @@
 package week2;
 
 
+import java.util.HashMap;
+import java.util.Stack;
+
 public class Problems {
     static class ListNode {
         int val;
@@ -99,7 +102,125 @@ public class Problems {
         return result;
     }
 
-    
+    public ListNode deleteDuplicates(ListNode head) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+        ListNode slow = new ListNode(0, head);
+        ListNode fast = head;
+        ListNode copy = slow;
+        int count = 0;
+        while (fast != null) {
+            if (fast.next == null) {
+                if (count != 0) {
+                    slow.next = null;
+                }
+            } else if (fast.next.val == fast.val) {
+                count++;
+            } else if (fast.next.val != fast.val && count != 0) {
+                slow.next = fast.next;
+                count = 0;
+            } else {
+                count = 0;
+                slow = fast;
+            }
+            fast = fast.next;
+        }
+        return copy.next;
+    }
+    // TODO: Problem 6
+
+    public void reorderList(ListNode head) {
+        if (head == null || head.next == null || head.next.next == null) {
+            return;
+        }
+        ListNode slow = head;
+        ListNode fast = head;
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        ListNode secondHalf = slow;
+
+
+        // reverse second half
+        ListNode prev = null;
+        ListNode cur = secondHalf;
+        while (cur != null) {
+            ListNode temp = cur.next;
+            cur.next = prev;
+            prev = cur;
+            cur = temp;
+            System.out.println(prev.val);
+        }
+        ListNode reversed = prev;
+        ListNode result = new ListNode(0);
+        ListNode first = head;
+        ListNode temp = head;
+        while (reversed.next != null) {
+            temp = first.next;
+            first.next = reversed;
+            first = temp;
+            temp = reversed.next;
+            reversed.next = first;
+            reversed = temp;
+        }
+    }
+    // TODO: Problem 8
+
+    public ListNode swapPairs(ListNode head) {
+        ListNode temp = new ListNode(0, head);
+        ListNode result = temp;
+        while (temp.next != null && temp.next.next != null) {
+            ListNode current = temp.next;
+            ListNode current2 = temp.next.next;
+            temp.next = current2;
+            current.next = temp.next.next;
+            temp.next.next = current;
+            temp = temp.next.next;
+        }
+        return result.next;
+    }
+
+    public ListNode removeElements(ListNode head, int val) {
+        ListNode temp = new ListNode(0, head);
+        ListNode result = temp;
+        while (temp != null && temp.next != null) {
+            if (temp.next.val == val) {
+                temp.next = temp.next.next;
+            } else {
+                temp = temp.next;
+            }
+
+        }
+        return result.next;
+    }
+
+    public boolean isValid(String s) {
+        HashMap<Character, Character> map = new HashMap<Character, Character>();
+        map.put(')', '(');
+        map.put(']', '[');
+        map.put('}', '{');
+        Stack<Character> stack = new Stack<Character>();
+        for (int i = 0; i < s.length(); i++) {
+            if (map.containsKey(s.charAt(i))) {
+                if (stack.isEmpty()) {
+                    return false;
+                } else {
+                    char c = stack.pop();
+                    if (map.get(s.charAt(i)) != c) {
+                        return false;
+                    }
+                }
+            } else {
+                stack.push(s.charAt(i));
+            }
+        }
+        return stack.isEmpty();
+    }
+
+
     public static void main(String[] args) {
         ListNode a = new ListNode(1);
         ListNode b = new ListNode(2);
